2d0972b42b44c5e00ae60aea9c28637ae3e788a2
thejayeshsoni <sonijayesh12345@gmail.com>
2023-07-04 22:17:19 +0530

    - changes were made into App.js file

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from REACT"
-); // core react thing
+const heading = React.createElement("div", { id: "heading" }, [
+  React.createElement("h1", {}, "Hello from REACT"),
+  React.createElement("h2", {}, "Changes made by Jayesh Soni"),
+]); // core react thing
 
 const root = ReactDOM.createRoot(document.getElementById("parent"));
 
