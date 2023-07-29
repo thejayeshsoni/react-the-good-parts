87f869bfb960aa669276623d09f0900cf086e516
Jayesh Soni <sonijayesh12345@gmail.com>
2023-07-29 13:53:01 +0530

    - jsx added

--- App.js
+++ App.js
@@ -3,5 +3,10 @@ import ReactDOM from "react-dom/client";
 
 // React.createElement() => Object => HTMLElement (render)
 const heading = React.createElement("h1", { id: "heading" }, "Hello from React..!");
+console.log(heading)
+
+const jsxHeading = <h1 id="heading">Hello from JSX</h1>;
+console.log(jsxHeading)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
