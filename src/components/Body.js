5235223e757543482e424bf89b977663fb3e19df
Jayesh Soni <sonijayesh12345@gmail.com>
2023-07-29 19:57:26 +0530

    - mockData for restaurents used with useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,47 +1,47 @@
 import { useState } from "react";
 import RestaurentCard from "./RestaurentCard";
-// import resList from "../utils/mockData";
+import resList from "../utils/mockData";
 
 const Body = () => {
   // Normal Variable
-  let listOfRestaurents = [
-    {
-      info: {
-        id: "45607",
-        name: "Domino's Pizza",
-        cloudinaryImageId: "gzobptvged4mzsk4vnfx",
-        areaName: "Sodala",
-        costForTwo: "₹400 for two",
-        cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
-        avgRating: 4.9,
-      },
-    },
-    {
-      info: {
-        id: "45608",
-        name: "Romino's Pizza",
-        cloudinaryImageId: "gzobptvged4mzsk4vnfx",
-        areaName: "Sodala",
-        costForTwo: "₹400 for two",
-        cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
-        avgRating: 3.9,
-      },
-    },
-    {
-      info: {
-        id: "45609",
-        name: "KFC",
-        cloudinaryImageId: "gzobptvged4mzsk4vnfx",
-        areaName: "Sodala",
-        costForTwo: "₹400 for two",
-        cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
-        avgRating: 4.1,
-      },
-    },
-  ];
+  // let listOfRestaurents = [
+  //   {
+  //     info: {
+  //       id: "45607",
+  //       name: "Domino's Pizza",
+  //       cloudinaryImageId: "gzobptvged4mzsk4vnfx",
+  //       areaName: "Sodala",
+  //       costForTwo: "₹400 for two",
+  //       cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+  //       avgRating: 4.9,
+  //     },
+  //   },
+  //   {
+  //     info: {
+  //       id: "45608",
+  //       name: "Romino's Pizza",
+  //       cloudinaryImageId: "gzobptvged4mzsk4vnfx",
+  //       areaName: "Sodala",
+  //       costForTwo: "₹400 for two",
+  //       cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+  //       avgRating: 3.9,
+  //     },
+  //   },
+  //   {
+  //     info: {
+  //       id: "45609",
+  //       name: "KFC",
+  //       cloudinaryImageId: "gzobptvged4mzsk4vnfx",
+  //       areaName: "Sodala",
+  //       costForTwo: "₹400 for two",
+  //       cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+  //       avgRating: 4.1,
+  //     },
+  //   },
+  // ];
 
   //Local State Variable
-  const [listOfRes, setListOfRes] = useState(listOfRestaurents);
+  const [listOfRes, setListOfRes] = useState(resList);
 
   return (
     <div className="body">
