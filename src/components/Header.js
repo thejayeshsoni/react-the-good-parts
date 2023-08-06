132c1e5c6fc228eb61722f864de5bf914dc1d287
Jayesh Soni <sonijayesh12345@gmail.com>
2023-08-06 09:27:06 +0530

    - onlineStatus added on Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
+  const onlineStatus = useOnlineStatus();
   console.log("Header rendered");
+
   useEffect(() => {
     console.log("useEffect called");
   }, [btnName]);
@@ -19,6 +22,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status : {onlineStatus ? "🟢" : "🔴"}</li>
           <li>
             <Link to="/">Home</Link>
           </li>
